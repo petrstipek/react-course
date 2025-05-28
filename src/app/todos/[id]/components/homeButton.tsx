3c53dbf291268a2f3020b193940029ca82fec902
Petr Stipek <stipepet@fit.cvut.cz>
2025-05-28 14:12:42 +0200

    searchParams for detail

--- src/app/todos/[id]/components/homeButton.tsx
+++ src/app/todos/[id]/components/homeButton.tsx
@@ -1,11 +1,14 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export const HomeButton = () => {
     const router = useRouter();
     function handleRedirect() {
         router.push("/");
     }
+    const searchParams = useSearchParams();
+    const search = searchParams.get("search");
+    console.log(search);
     return (
         <>
             <button onClick={handleRedirect} className="home-button">
